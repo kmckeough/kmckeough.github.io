ac44d71cefeac6bba2c2ca7cb7ec7402ac05c6d7
kmckeough <katy.mckeough@gmail.com>
2019-11-08 09:51:20 -0500

    initial commit

---
+++ index.html
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Scrolling Nav - Start Bootstrap Template</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/scrolling-nav.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">Roo Croo Insights</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#who">Who?</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#when">When?</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#where">Where?</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <header class="main-header text-white">
+    <div class="container text-center">
+      <h1>Mom's Spaghetti</h1>
+      <p class="lead">Bonnaroo croo insights</p>
+    </div>
+  </header>
+
+  <br>
+  <br>
+  <div class="text-body text-white">
+    <h1 class="text-white">Croo members:</h1>
+    <p id="total" class="text-white">0</p>
+  </div>
+
+  <section id="who", class="graphic-body">
+    <h2>Who?</h2>
+    <div class="container" , id="network">
+    </div>
+  </section>
+  <section id="when", class="graphic-body">
+    <h2>When?</h2>
+    <div class="container" , id="timeline">
+    </div>
+  </section>
+  <section id="where", class="graphic-body">
+    <h2>Where?</h2>
+    <div class="container" , id="map">
+    </div>
+  </section>
+
+
+  <footer class="py-5 footer">
+      <img src="https://cdn-3-www.bonnaroo.com/wp/wp-content/themes/forte-child/img/2019/b19-footer.svg" alt="" class="o-footer__landscape">
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom JavaScript for this theme -->
+  <script src="js/scrolling-nav.js"></script>
+
+  <!-- D3 -->
+  <script src="https://d3js.org/d3.v4.min.js"></script>
+
+  <!-- Custom JavaScript-->
+  <script src="js/network.js"></script>
+  <script src="js/total.js"></script>
+
+
+</body>
+
+</html>
---
+++ js/network.js
@@ -0,0 +1,135 @@
+var width = document.getElementById("who").clientWidth,
+    height = 600;
+
+var svg = d3.select("#network").append("svg")
+    .attr("id","networksvg")
+    .attr("width", width)
+    .attr("height", height);
+
+
+
+
+
+
+// Load data
+d3.json("data/connections.json", function(data) {
+
+
+    console.log(data);
+
+
+    // 1) INITIALIZE FORCE-LAYOUT
+    var force = d3.forceSimulation(data.nodes)
+        .force("charge", d3.forceManyBody().strength(-50))
+        .force("link", d3.forceLink(data.links).distance(100))
+        .force("center", d3.forceCenter().x(width/2).y(height/2))
+        .alphaDecay(0.01);
+
+
+    // 2a) DEFINE 'NODES' AND 'EDGES'
+
+
+    var edges = svg.selectAll(".edge")
+        .data(data.links)
+        .enter().append("line")
+        .style("stroke","#ccc")
+        .style("stroke-width",4);
+
+    var node = svg.selectAll(".node")
+        .data(data.nodes)
+        .enter().append("circle")
+        .attr("class", "node")
+        .attr("r", 30)
+        .attr("fill", function(d){
+            if(d.city == "New York, NY"){
+                return "#FF0080"
+            }else if( d.city == "Boston, MA"){
+                return "#d9df00"
+            }else if( d.city == "Washington DC"){
+                return "#7a41ff"
+            }else if( d.city == "Austin, TX"){
+                return "#fe6b00"
+            }else if( d.city == "San Fransisco, CA"){
+                return "#009fdf"
+            }else if( d.city =="Philadelphia, PA"){
+                return "#84e12b"
+            }else{
+                return
+            }
+
+
+
+        });
+
+
+    var label = svg.selectAll(".nodelabel")
+        .data(data.nodes)
+        .enter().append("text")
+        .attr("class", "nodelabel")
+        .text(function(d) { return d.name; })
+        .attr("fill", function(d) {
+            if (d.city == "Boston, MA") {
+                return "#343a40"
+            } else if (d.city == "Philadelphia, PA"){
+                return "#343a40"
+            }else{
+                return "#ffffff"
+            }
+        });
+
+    // 2b) START RUNNING THE SIMULATION
+    force.on("tick", function() {
+
+        // Update labels
+        label
+            .attr("x", function(d) { return d.x; })
+            .attr("y", function(d) { return d.y; });
+        // Update node coordinates
+        node
+            .attr("cx", function(d) { return d.x; })
+            .attr("cy", function(d) { return d.y; });
+
+        // Update edge coordinates
+        edges
+            .attr("x1",function(d){return d.source.x;})
+            .attr("y1",function(d){ return d.source.y;})
+            .attr("x2",function(d){return d.target.x;})
+            .attr("y2", function(d){ return d.target.y;})
+    });
+
+
+    // Dragging
+
+    function dragStarted(d){
+        if(!d3.event.active) force.alphaTarget(0.3).restart();
+        d.fx=d.x;
+        d.fy=d.y;
+    };
+
+    function dragging(d){
+        d.fx = d3.event.x;
+        d.fy = d3.event.y;
+    };
+
+    function dragEnded(d){
+        if(!d3.event.active) force.alphaTarget(0);
+        d.fx = null;
+        d.fy = null;
+    };
+
+    node.call(d3.drag()
+        .on("start", dragStarted)
+        .on("drag", dragging)
+        .on("end", dragEnded));
+
+    label.call(d3.drag()
+        .on("start", dragStarted)
+        .on("drag", dragging)
+        .on("end", dragEnded));
+
+
+
+
+
+
+});
